20355a302f2c23aeeefe9081a981b3689b92a7e8
edsonarios <edsonrios9@gmail.com>
2024-05-27 17:58:06 -0400

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   await app.listen(3000)
-  app.enableCors()
+  console.log('Enable cors')
+  app.enableCors({
+    origin: '*',
+    methods: 'ALL',
+    allowedHeaders:
+      'Content-Type, Accept, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token',
+  })
 }
 bootstrap()
