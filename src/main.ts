3da116b744756e53aed5b49d6ee5878de38311a0
edsonarios <edsonrios9@gmail.com>
2024-05-27 18:11:50 -0400

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   await app.listen(3000)
-  console.log('Enable cors2')
+  console.log('Enable cors3')
   app.enableCors({
-    origin: '*',
-    methods: 'ALL',
+    origin: ['*'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   })
 }
